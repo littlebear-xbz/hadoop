f90dbf43b5d8f01fc5b14e4c80560982b7dbda02
littlebear <littlebear.xbz@qq.com>
2017-11-16 12:54:54 +0800

    kafka streaming 测试

--- sparkbyME/test/kafka_json_sql.py
+++ sparkbyME/test/kafka_json_sql.py
@@ -2,14 +2,18 @@
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
+from pyspark.sql import SparkSession
+from pyspark.conf import SparkConf
+import os
 import sys
 import json
 
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+spark = SparkSession.builder.master("local[2]").appName("kafkaTest").config(conf=SparkConf()).getOrCreate()
 
-sc = SparkContext("local[2]", "kafkaTest")
+sc = spark.sparkContext
 ssc = StreamingContext(sc, 2)
 zookeeper = "jp-bigdata-02:2181, jp-bigdata-03:2181, jp-bigdata-06:2181"
 topic = "ltest"
@@ -20,11 +24,13 @@ initDstream = KafkaUtils.createStream(ssc=ssc, zkQuorum=zookeeper, groupId="grou
 df = initDstream.map(lambda x: x[1].replace(u""""cltz":"",""", ""))
 
 
-def processjson(i):
-    jsonmessage = json.loads(i)
-    return jsonmessage['take']
+def processRDD(rdd):
+    if rdd.isEmpty() is False:
+        showschem = spark.read.json(rdd)
+        showschem.select("take").show()
 
 
-df.map(processjson).pprint()
+df.foreachRDD(processRDD)
+
 ssc.start()
 ssc.awaitTermination()
